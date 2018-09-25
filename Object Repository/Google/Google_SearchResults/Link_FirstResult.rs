640c1a84e912296245f0b5f504c478d9762e662f
Julien <julien.gossins@outlook.com>
2018-09-25 09:50:27 +0200

    pom.xml

--- Object Repository/Google/Google_SearchResults/Link_FirstResult.rs
+++ Object Repository/Google/Google_SearchResults/Link_FirstResult.rs
@@ -10,8 +10,12 @@
          <value>.rc>.r>a</value>
       </entry>
       <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;r&quot;]/a</value>
+      </entry>
+      <entry>
          <key>XPATH</key>
-         <value>//*[@id=&quot;rso&quot;]/div[2]/div/div/div/h3/a</value>
+         <value>//*[@id=&quot;rc&quot;]/*[@id=&quot;r&quot;]/a</value>
       </entry>
    </selectorCollection>
    <selectorMethod>CSS</selectorMethod>
@@ -21,7 +25,8 @@
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
-      <value>h3</value>
+      <value>a
+</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -31,18 +36,18 @@
       <value>LC20lb</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
       <value>République française - France — Wikipédia</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>false</isSelected>
+      <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;rso&quot;)/div[@class=&quot;bkWMgd&quot;]/div[@class=&quot;g&quot;]/div[1]/div[@class=&quot;rc&quot;]/div[@class=&quot;r&quot;]/a[1]/h3[@class=&quot;LC20lb&quot;]</value>
+      <value>//*[@id=&quot;r&quot;]/a</value>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
--- Test Suites/Suite_Google.ts
+++ Test Suites/Suite_Google.ts
@@ -4,7 +4,7 @@
    <name>Suite_Google</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-20T13:26:59</lastRun>
+   <lastRun>2018-09-24T16:31:17</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
